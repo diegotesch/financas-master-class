de13679f62b609064ab51d29ed5b0f05b9a5c9b2
diegotesch <diego.tesch@gmail.com>
2020-08-02 15:25:35 -0300

    componente de errors

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -5,11 +5,13 @@ import { RouterModule } from '@angular/router';
 
 import { BreadCrumbComponent } from './components/bread-crumb/bread-crumb.component';
 import { PageHeaderComponent } from './components/page-header/page-header.component';
+import { FormFieldErrorComponent } from './components/form-field-error/form-field-error.component';
 
 @NgModule({
   declarations: [
     BreadCrumbComponent,
-    PageHeaderComponent
+    PageHeaderComponent,
+    FormFieldErrorComponent
   ],
   imports: [
     CommonModule,
@@ -22,7 +24,8 @@ import { PageHeaderComponent } from './components/page-header/page-header.compon
     RouterModule,
 
     BreadCrumbComponent,
-    PageHeaderComponent
+    PageHeaderComponent,
+    FormFieldErrorComponent
   ]
 })
 export class SharedModule { }
